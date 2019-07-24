9582914eea067696bff0307df70e3666ba59214e
Hakan Yildiz <hakanyildiz248@gmail.com>
2019-07-24 12:45:19 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import CatComponent from './CatComponent';
 import EinsteinQuoteComponent from './EinsteinQuoteComponent';
 import MouseComponent from './MouseComponent';
 
-class App extends Component {
+class App extends React.Component {
 	render() {
 		// your code in the return statement below!
 		return (
